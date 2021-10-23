603e677bc2691e5b223193be4a844dbccdfab47d
Roman Bereziuk <roma.physicist@gmail.com>
2021-10-23 20:41:58 +0300

    Create a basic reducer for adding and removing tasks

---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+const initialState = [];
+let lastId = 0;
+
+// Reducer - pure function
+// When the app starts running, redux calls reducer for setup the store
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          isComplete: false,
+        },
+      ];
+    case 'DELETE_TASK':
+      return state.filter(task => task.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
