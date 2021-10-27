c996a72a7926c34dd8aac992192399c5f0f01340
Roman Bereziuk <roma.physicist@gmail.com>
2021-10-27 22:25:31 +0300

    Use createReducer from Redux Toolkit package

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,35 +1,30 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
-// Action creators
+// --- Action creators ---
 export const addTask = createAction('ADD_TASK');
 export const completeTask = createAction('COMPLETE_TASK');
 export const deleteTask = createAction('DELETE_TASK');
 
 // --- Reducer ---
-const initialState = [];
 let lastId = 0;
 
 // Reducer - pure function
 // When the app starts running, redux calls reducer for setup the store
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case addTask.type:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          isComplete: false,
-        },
-      ];
-    case completeTask.type:
-      return state.map(task =>
-        task.id === action.payload.id ? { ...task, isComplete: true } : task
-      );
-
-    case deleteTask.type:
-      return state.filter(task => task.id !== action.payload.id);
-    default:
-      return state;
-  }
-}
+export default createReducer([], {
+  // key: value
+  // actions: functions (event => event handler)
+  [addTask.type]: (tasks, action) => {
+    tasks.push({
+      id: ++lastId,
+      description: action.payload.description,
+      isComplete: false,
+    });
+  },
+  [completeTask.type]: (tasks, action) => {
+    const index = tasks.findIndex(task => task.id === action.payload.id);
+    tasks[index].isComplete = true;
+  },
+  [deleteTask.type]: (tasks, action) => {
+    return tasks.filter(task => task.id !== action.payload.id);
+  },
+});
