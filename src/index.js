b1ee9f2e34bec3578fa86700d35a88932d63258b
Roman Bereziuk <roma.physicist@gmail.com>
2021-11-02 19:13:37 +0200

    Use createSlice from Redux Toolkit package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import store from './store';
-import { addTask, completeTask, deleteTask } from './store/tasks';
+import { taskAdded, taskCompleted, taskDeleted } from './store/tasks';
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store changed 🗂', store.getState());
 });
 
-store.dispatch(addTask({ description: 'Clean the room' }));
-store.dispatch(addTask({ description: 'Read a book' }));
+store.dispatch(taskAdded({ description: 'Clean the room' }));
+store.dispatch(taskAdded({ description: 'Read a book' }));
 
-store.dispatch(completeTask({ id: 1 }));
+store.dispatch(taskCompleted({ id: 1 }));
 
-store.dispatch(deleteTask({ id: 1 }));
+store.dispatch(taskDeleted({ id: 2 }));
 
 unsubscribe();
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,30 +1,33 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
-
-// --- Action creators ---
-export const addTask = createAction('ADD_TASK');
-export const completeTask = createAction('COMPLETE_TASK');
-export const deleteTask = createAction('DELETE_TASK');
+import { createSlice } from '@reduxjs/toolkit';
 
 // --- Reducer ---
 let lastId = 0;
 
 // Reducer - pure function
 // When the app starts running, redux calls reducer for setup the store
-export default createReducer([], {
-  // key: value
-  // actions: functions (event => event handler)
-  [addTask.type]: (tasks, action) => {
-    tasks.push({
-      id: ++lastId,
-      description: action.payload.description,
-      isComplete: false,
-    });
-  },
-  [completeTask.type]: (tasks, action) => {
-    const index = tasks.findIndex(task => task.id === action.payload.id);
-    tasks[index].isComplete = true;
-  },
-  [deleteTask.type]: (tasks, action) => {
-    return tasks.filter(task => task.id !== action.payload.id);
+const slice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    // actions => action handlers
+    taskAdded: (tasks, action) => {
+      tasks.push({
+        id: ++lastId,
+        description: action.payload.description,
+        isComplete: false,
+      });
+    },
+    taskCompleted: (tasks, action) => {
+      const index = tasks.findIndex(task => task.id === action.payload.id);
+      tasks[index].isComplete = true;
+    },
+    taskDeleted: (tasks, action) => {
+      return tasks.filter(task => task.id !== action.payload.id);
+    },
   },
 });
+
+export const { taskAdded, taskCompleted, taskDeleted } = slice.actions;
+export default slice.reducer;
+
+console.log(slice);
